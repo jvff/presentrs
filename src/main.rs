9395d01caa85e5c2d31079083acca55e644da3dc
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-07-07 17:51:49 +0000

    Create initial Hello World server using Actix-Web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate actix_web;
+
+use actix_web::{server, App, HttpRequest};
+
+fn index(_: HttpRequest) -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("0.0.0.0:8080")
+        .unwrap()
+        .run();
 }
