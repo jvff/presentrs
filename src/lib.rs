05c3fae6cacccfab002244b33ce3e89d576e542e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-07-15 12:47:38 +0000

    Move server code into a `server` feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+#[cfg(feature = "server")]
 extern crate actix_web;
+#[cfg(feature = "server")]
 #[macro_use]
 extern crate failure;
 
+#[cfg(feature = "server")]
 mod server;
 
+#[cfg(feature = "server")]
 pub use server::{Server, ServerStartError};
