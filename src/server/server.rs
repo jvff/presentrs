9420afacadec7ac92e7d7d3b094f5aa9d2cac0f0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-07-19 20:02:26 +0000

    Allow configuring static file directory

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,16 +1,31 @@
 use std::io;
 use std::net::ToSocketAddrs;
+use std::path::PathBuf;
 
 use actix_web::{self, App};
 
-pub struct Server;
+pub struct Server {
+    static_dir: PathBuf,
+}
 
 impl Server {
+    pub fn new<P>(static_dir: P) -> Self
+    where
+        P: Into<PathBuf>,
+    {
+        Server {
+            static_dir: static_dir.into(),
+        }
+    }
+
     pub fn run<T>(&mut self, address: T) -> Result<(), ServerStartError>
     where
         T: ToSocketAddrs,
     {
-        let server = actix_web::server::new(Self::create_app);
+        let static_dir = self.static_dir.clone();
+        let server = actix_web::server::new(move || {
+            Self::create_app(static_dir.clone())
+        });
 
         server
             .bind(address)
@@ -20,8 +35,8 @@ impl Server {
         Ok(())
     }
 
-    fn create_app() -> App<()> {
-        let static_handler = actix_web::fs::StaticFiles::new("static");
+    fn create_app(static_dir: PathBuf) -> App<()> {
+        let static_handler = actix_web::fs::StaticFiles::new(static_dir);
 
         App::new().handler("/", static_handler)
     }
