79dc9b13203991a66418bc65205c27af357ee0b8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-07-19 11:17:05 +0000

    Shrink slide a little to avoid scrollbars

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -39,7 +39,8 @@ impl Presentrs {
         let width = window.inner_width() as f64;
         let height = window.inner_height() as f64;
 
-        self.slide_size.resize_to_fit_in(width, height);
+        self.slide_size
+            .resize_to_fit_in(width * 0.98, height * 0.98);
     }
 
     fn on_key_down(event: KeyDownEvent) -> Message {
