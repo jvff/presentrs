f4647bb71f9feaf6ea03175a4f4289c80954cc6f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-24 20:52:48 +0000

    Remove server implementation
    It was a very simple static file server, so it's easy to replace. This
    also removes the dependency on `actix`.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,9 +1,5 @@
 mod notes;
 mod slides;
 
-mod server;
-
 pub use self::notes::{Notes, NotesError};
 pub use self::slides::{Slides, SlidesError};
-
-pub use self::server::{Server, ServerStartError};
