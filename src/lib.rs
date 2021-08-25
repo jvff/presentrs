a1e0297cc84cff6a0667f0d4b8112fbefa1d35c9
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-25 19:49:57 +0000

    Add back the `client` module
    It has been updated and should be ready for some initial usage.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+#[cfg(target_family = "wasm")]
+mod client;
+
 #[cfg(not(target_family = "wasm"))]
 mod server;
+
+#[cfg(target_family = "wasm")]
+pub use crate::client::Presentrs;
 
 #[cfg(not(target_family = "wasm"))]
 pub use crate::server::{Notes, NotesError, Slides, SlidesError};
