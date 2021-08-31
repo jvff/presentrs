4f834262cbc7c0af56ed6bcb1e7ea8045fb37b75
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-31 15:14:44 +0000

    Refactor to use new `NavigationButton`s
    Use the new component and style the footer to support it.

--- src/client/navigation.rs
+++ src/client/navigation.rs
@@ -1,18 +1,13 @@
-use yew::prelude::*;
+use {
+    super::navigation_button::{Direction, NavigationButton, Target},
+    yew::prelude::*,
+};
 
 pub struct Navigation {
-    on_previous_slide: Option<Callback<MouseEvent>>,
-    on_previous_step: Option<Callback<MouseEvent>>,
-    on_next_step: Option<Callback<MouseEvent>>,
-    on_next_slide: Option<Callback<MouseEvent>>,
-}
-
-impl Navigation {
-    fn build_callback(
-        callback: Option<Callback<()>>,
-    ) -> Option<Callback<MouseEvent>> {
-        callback.map(|callback| callback.reform(|_| ()))
-    }
+    on_previous_slide: Option<Callback<()>>,
+    on_previous_step: Option<Callback<()>>,
+    on_next_step: Option<Callback<()>>,
+    on_next_slide: Option<Callback<()>>,
 }
 
 impl Component for Navigation {
@@ -21,12 +16,10 @@ impl Component for Navigation {
 
     fn create(properties: Self::Properties, _: ComponentLink<Self>) -> Self {
         Navigation {
-            on_previous_slide: Self::build_callback(
-                properties.on_previous_slide,
-            ),
-            on_previous_step: Self::build_callback(properties.on_previous_step),
-            on_next_step: Self::build_callback(properties.on_next_step),
-            on_next_slide: Self::build_callback(properties.on_next_slide),
+            on_previous_slide: properties.on_previous_slide,
+            on_previous_step: properties.on_previous_step,
+            on_next_step: properties.on_next_step,
+            on_next_slide: properties.on_next_slide,
         }
     }
 
@@ -35,39 +28,48 @@ impl Component for Navigation {
     }
 
     fn change(&mut self, properties: Self::Properties) -> ShouldRender {
-        self.on_previous_slide =
-            Self::build_callback(properties.on_previous_slide);
-        self.on_previous_step =
-            Self::build_callback(properties.on_previous_step);
-        self.on_next_step = Self::build_callback(properties.on_next_step);
-        self.on_next_slide = Self::build_callback(properties.on_next_slide);
+        self.on_previous_slide = properties.on_previous_slide;
+        self.on_previous_step = properties.on_previous_step;
+        self.on_next_step = properties.on_next_step;
+        self.on_next_slide = properties.on_next_slide;
 
         true
     }
 
     fn view(&self) -> Html {
         html! {
-            <div style={
-                "position: absolute;
-                 bottom: 0;
-                 width: 97%;"
-            }>
-                <form style={
-                    "margin-left: auto; margin-right: auto;"
-                }>
-                    <button onclick=&self.on_previous_slide>
-                        {"Previous slide"}
-                    </button>
-                    <button onclick=&self.on_previous_step>
-                        {"Previous step"}
-                    </button>
-                    <button onclick=&self.on_next_step>
-                        {"Next step"}
-                    </button>
-                    <button onclick=&self.on_next_slide>
-                        {"Next slide"}
-                    </button>
-                </form>
+            <div style="
+                position: absolute;\
+                left: 0;\
+                right: 0;\
+                bottom: 0;\
+            ">
+                <div style="\
+                    margin-left: auto;\
+                    margin-right: auto;\
+                    display: table;\
+                ">
+                    <NavigationButton
+                        direction=Direction::Backward
+                        on_click=&self.on_previous_slide
+                        target=Target::Slide
+                        />
+                    <NavigationButton
+                        direction=Direction::Backward
+                        on_click=&self.on_previous_step
+                        target=Target::Step
+                        />
+                    <NavigationButton
+                        direction=Direction::Forward
+                        on_click=&self.on_next_step
+                        target=Target::Step
+                        />
+                    <NavigationButton
+                        direction=Direction::Forward
+                        on_click=&self.on_next_slide
+                        target=Target::Slide
+                        />
+                </div>
             </div>
         }
     }
